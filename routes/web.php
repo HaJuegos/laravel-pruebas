80a392da9140597a00b4bae60c1065a03a61c7f3
HaJuegos <hagatoclips69@gmail.com>
2024-10-17 17:17:18 -0500

    Fix data

--- routes/web.php
+++ routes/web.php
@@ -1,20 +1,10 @@
 <?php
 
-use App\Http\Controllers\FormCuadraticaController;
 use App\Http\Controllers\PostsManagerController;
 use Illuminate\Support\Facades\Route;
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
-
-Route::get('/', [FormCuadraticaController::class, 'viewForm']);
-Route::post('/total', [FormCuadraticaController::class, 'FormComplete'])->name(
-    'viewForm',
-);
-
-Route::get('/prueba', [PostsManagerController::class, 'index']);
-Route::post('/prueba', [
+Route::get('/', [PostsManagerController::class, 'index']);
+Route::post('/', [
     PostsManagerController::class,
     'resultadosIndex',
 ])->name('getData');
